ca9fbef4e69d59e2923e96787936518596c0a913
root <root@DESKTOP-C7MNT8H.localdomain>
2018-10-09 14:37:49 +0800

    Project initialization

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+	<link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+	<title>Roast</title>
+
+	<script type="text/javascript">
+		window.Laravel = <?php echo json_encode([
+			'csrfToken' => csrf_token(),
+		]); ?>
+	</script>
+</head>
+<body>
+	<div id="app">
+		<router-view></router-view>
+	</div>
+
+	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
